9b6d1ed837768d760ff46ed17d07db644a35ed8a
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-08-18 21:48:42 -0300

    lógica movieServices

---
+++ src/api/MovieService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const BASE_URL = "https://www.themoviedb.org/3/"
+const API_KEY = "a3c584657d91bc437534a5d3b5acfb72";
+
+const whithBaseUrl = (path) => `${BASE_URL}${path}api_key=${API_KEY}`;
+
+export class MovieService {
+    static getMovies() { //esse static me obriga a usar o MovieService para chamar qualquer um
+        //dos metodos descritos abaixo
+        return axios(whithBaseUrl("movie/popular"))
+    }
+    static getMovieDetails(id){
+        return axios(whithBaseUrl(`movie/${id}`));
+    }
+    static searchMovies(movie){
+        return axios(whithBaseUrl("search/movie") + `&query=${movie}`);
+    }
+}
