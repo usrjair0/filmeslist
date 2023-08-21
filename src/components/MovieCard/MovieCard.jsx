1891071f4c73b2cb1e7aaf23fa57a464f9ebdb74
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-08-21 19:29:06 -0300

    movie card

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,6 +1,19 @@
 const MovieCard = () => {
   return (
-    <div>MovieCard</div>
+    <div className="MovieCard">
+      <div className="movie-image">
+        <img
+          src={`https://image.tmdb.org/t/p/w1280${movieProp.poster_path}`}
+          alt={movieProp.title}
+        />
+      </div>
+      <div className="movie-description">
+        <h3>{movieProp.title}</h3>
+        <Link to={`/movie/${movieProp.id}`} className="btn-details">
+          Ver Detalhes
+        </Link>
+      </div>
+    </div>
   )
 }
 
