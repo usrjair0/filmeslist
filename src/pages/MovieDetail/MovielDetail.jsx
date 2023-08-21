a9203e38a171c5bba063989f3359fc19b3963100
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-08-21 19:26:38 -0300

    movie detail

--- src/pages/MovieDetail/MovielDetail.jsx
+++ src/pages/MovieDetail/MovielDetail.jsx
@@ -1,6 +1,37 @@
 const MovielDetail = () => {
   return (
-    <div>MovielDetail</div>
+    <section className="MovieDetail">
+      <div className="MovieDetail__container">
+        <div className="MovieDetail__col">
+          <h1 className="MovieDetail__title">{movie.title}</h1>
+          <div className="MovieDetail__image">
+            <img
+              src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="MovieDetail__col">
+          <div className="MovieDetail__details">
+            <div className="MovieDetail__detail">
+              <span>Budget:</span> {movie.budget}
+            </div>
+            <div className="MovieDetail__detail">
+              <span>Original language:</span> {movie.original_language}
+            </div>
+            <div className="MovieDetail__detail">
+              <span>Popularity:</span> {movie.popularity}
+            </div>
+            <div className="MovieDetail__detail">
+              <span>Overview:</span> {movie.overview}
+            </div>
+          </div>
+          <Link to={"/"} className="MovieDetail__button">
+            Voltar
+          </Link>
+        </div>
+      </div>
+    </section>
   )
 }
 
