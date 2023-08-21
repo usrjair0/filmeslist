023d62593d989dc6785befbd6a12daa0ad90a2b0
usrjair0 <juniorsilvasobrinho1999@gmail.com>
2023-08-21 19:09:40 -0300

    added list filmes to homejsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,20 +5,27 @@ const Home = () => {
   const [movies, setMovies] = useState([])
 
   async function getMovies (){
-    const data = await MovieService.getMovies()
-    console.log(data)
+    const { 
+      data: { results }
+    } = await MovieService.getMovies()
+    console.log(results)
+
+    setMovies(results)
   }
 
   useEffect(() => {
     getMovies();
-  })
+  }, [])
   
 
   return (
     <section className="Home">
-      {
-
-      }
+      {movies.map((movie)=>(
+        <div key={movie.id}>
+          <h1>{movie.title}</h1>
+          <h2>{movie.vote_average}</h2>
+        </div>
+    ))}
     </section>
   )
 }
